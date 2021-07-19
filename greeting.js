9be85ef007386330d85bc5273557789016cb6a61
yunseo323 <jokani3244833@gmail.com>
2021-07-19 19:45:24 +0900

    add local-storage

---
+++ greeting.js
@@ -0,0 +1,14 @@
+const form = document.querySelector(".js-form"),
+    input = form.querySelector("input");
+
+const USER_LS = "currentUser";
+function loadName(){
+    const currentUser = localStorage.getItem
+}
+
+
+function init(){
+
+}
+
+init();
--- js-nomadCoder/greeting.js
+++ js-nomadCoder/greeting.js
@@ -1,8 +1,43 @@
 const form = document.querySelector(".js-form"),
-    input = form.querySelector("input");
+    input = form.querySelector("input"),
+    greeting = document.querySelector(".js-greetings");
 
+const USER_LS = "currentUser",
+    SHOWING_CN = "showing";
+
+function saveName(text){
+    localStorage.setItem(USER_LS,text);
+}
+
+function handleSubmit(event){
+    event.preventDefault();
+    const currentValue = input.value;
+    paintGreeting(currentValue);
+    saveName(currentValue);
+}
+
+function askForName(){
+    form.classList.add(SHOWING_CN);
+    form.addEventListener("submit",handleSubmit); //default 동작 막기
+}
+
+function paintGreeting(text){
+    form.classList.remove(SHOWING_CN); //form을 숨겨야 함
+    greeting.classList.add(SHOWING_CN);
+    greeting.innerText = `Hello ${text}`;
+}
+
+function loadName(){ // name을 불러옴
+    const currentUser = localStorage.getItem(USER_LS);
+    if(currentUser===null){
+        askForName();
+    }else{
+        paintGreeting(currentUser);
+    }
+
+}
 function init(){
-
+    loadName();
 }
 
 init();
